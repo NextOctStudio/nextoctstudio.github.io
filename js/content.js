efb2f1e92e23f3c71ea5dc9cb216528b28bcf6e0
Jansuoh <161670831+Jansuoh@users.noreply.github.com>
2024-12-21 14:42:01 +0800

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,8 +3,9 @@ window.onload=function(){
 	if (width <= 1400) {
 		document.getElementById("post-list").style.setProperty('display', 'none');
 		document.getElementById("post-section").style.setProperty('width', '90vw');
-	} else {
+	} else if (width >= 1400) {
 		document.getElementById("post-section").style.setProperty('min-width', '48em');
+	} else {
 		document.getElementById("content-section").style.setProperty('width', '45em');
 	}
 }
