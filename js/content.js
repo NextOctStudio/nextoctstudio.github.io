9acba0821c5d0c94aedea4e547fc5382cd9cd06a
Jansuoh <161670831+Jansuoh@users.noreply.github.com>
2024-12-21 14:03:12 +0800

    fix

---
+++ js/content.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+	const width = window.innerWidth;
+	if (width <= 1400) {
+		document.getElementById("post-list").style.setProperty('display', 'none');
+		document.getElementById("post-section").style.setProperty('width', '90vw');
+	} else {
+		document.getElementById("content-section").style.setProperty('width', '45em');
+	}
+}
