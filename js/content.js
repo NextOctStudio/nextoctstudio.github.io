d092ed95db23196cc19d291b3cd1448c470928dd
Jansuoh <161670831+Jansuoh@users.noreply.github.com>
2024-12-24 12:54:36 +0800

    fix

--- js/content.js
+++ js/content.js
@@ -4,6 +4,20 @@ window.onload=function(){
 		document.getElementById("post-list").style.setProperty('display', 'none');
 		document.getElementById("post-section").style.setProperty('width', '90vw');
 	} else {
-		document.getElementById("post-section").style.setProperty('min-width', '48em');
+		document.getElementById("post-list").style.setProperty('display', 'block');
+		document.getElementById("post-section").style.setProperty('width', '48em');
 	}
 }
+
+function handleResize() {
+	const width = window.innerWidth;
+	if (width <= 1400) {
+		document.getElementById("post-list").style.setProperty('display', 'none');
+		document.getElementById("post-section").style.setProperty('width', '90vw');
+	} else {
+		document.getElementById("post-list").style.setProperty('display', 'block');
+		document.getElementById("post-section").style.setProperty('width', '48em');
+	}
+}
+
+window.addEventListener('resize', handleResize);
