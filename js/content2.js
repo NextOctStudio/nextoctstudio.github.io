fdf0a8179941a1f4a5e882641b434fcf59aa638c
Jansuoh <161670831+Jansuoh@users.noreply.github.com>
2024-12-25 17:52:17 +0800

    fix

--- js/content2.js
+++ js/content2.js
@@ -1,10 +1,12 @@
-const width = window.innerWidth;
-if (width <= 1400) {
-	document.getElementById("post-list").style.setProperty('display', 'none');
-	document.getElementById("content-section").style.setProperty('width', '88vw');
-} else {
-	document.getElementById("post-list").style.setProperty('display', 'block');
-	document.getElementById("content-section").style.setProperty('width', '45em');
+window.onload=function(){
+	const width = window.innerWidth;
+	if (width <= 1400) {
+		document.getElementById("post-list").style.setProperty('display', 'none');
+		document.getElementById("content-section").style.setProperty('width', '88vw');
+	} else {
+		document.getElementById("post-list").style.setProperty('display', 'block');
+		document.getElementById("content-section").style.setProperty('width', '45em');
+	}
 }
 
 function handleResize() {
