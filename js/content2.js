8d70b36e2f56aec2dce61bed13679a6ed094b086
Jansuoh <161670831+Jansuoh@users.noreply.github.com>
2024-12-25 18:04:01 +0800

    fix

--- js/content2.js
+++ js/content2.js
@@ -1,17 +1,13 @@
-window.onload=function(){
-	const width = window.innerWidth;
-	if (width <= 1400) {
-		document.getElementById("post-list").style.setProperty('display', 'none');
-		document.getElementById("content-section").style.setProperty('width', '88vw');
-	} else {
-		document.getElementById("post-list").style.setProperty('display', 'block');
-		document.getElementById("content-section").style.setProperty('width', '45em');
-	}
+if (window.innerWidth <= 1400) {
+	document.getElementById("post-list").style.setProperty('display', 'none');
+	document.getElementById("content-section").style.setProperty('width', '88vw');
+} else {
+	document.getElementById("post-list").style.setProperty('display', 'block');
+	document.getElementById("content-section").style.setProperty('width', '45em');
 }
 
 function handleResize() {
-	const width = window.innerWidth;
-	if (width <= 1400) {
+	if (window.innerWidth <= 1400) {
 		document.getElementById("post-list").style.setProperty('display', 'none');
 		document.getElementById("content-section").style.setProperty('width', '88vw');
 	} else {
--- js/footer.js
+++ js/footer.js
@@ -1,17 +1,14 @@
-window.onload=function(){
-	const width = window.innerWidth;
-	if (width <= 1400) {
-		document.getElementById("footer").style.setProperty('padding', '0 0');
-		document.getElementById("footer-main").style.setProperty('max-width', '100vw');
-	} else {
-		document.getElementById("footer").style.setProperty('padding', '0 5vw');
-		document.getElementById("footer-main").style.setProperty('max-width', '1208px');
-	}
+if (window.innerWidth <= 1400) {
+	document.getElementById("footer").style.setProperty('padding', '0 0');
+	document.getElementById("footer-main").style.setProperty('max-width', '100vw');
+} else {
+	document.getElementById("footer").style.setProperty('padding', '0 5vw');
+	document.getElementById("footer-main").style.setProperty('max-width', '1208px');
 }
 
+
 function handleResize() {
-	const width = window.innerWidth;
-	if (width <= 1400) {
+	if (window.innerWidth <= 1400) {
 		document.getElementById("footer").style.setProperty('padding', '0 0');
 		document.getElementById("footer-main").style.setProperty('max-width', '100vw');
 	} else {
